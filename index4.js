5b6d34588c1f953347715751c8b035724b7c72eb
TheCoderAishwarya <113344237+TheCoderAish@users.noreply.github.com>
2023-09-23 00:29:12 +0530

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,18 @@
+function stripes(){
+    let sizeW = Math.random() * 3;
+    let e = document.createElement('div');
+    e.classList.add('lines');
+    document.body.appendChild(e);
+
+    e.style.left = Math.random() * innerWidth + 'px';
+    e.style.width = 1 + sizeW + 'px';
+    e.style.background = 'hsl('+Math.floor(Math.random() * 60) +'deg, 100%, 50%)';
+    
+    setTimeout(function(){
+        document.body.removeChild(e)
+    },2000)
+}
+
+setInterval(function(){
+    stripes()
+},20);
